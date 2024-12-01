9fbf7bf7fcbfc86eaceee839f2ca41591603cc0f
dvillacinda <diegovillacinda.t@gmail.com>
2024-12-01 13:21:40 -0600

    updated gitignore

--- start-services.bat
+++
@@ -1,4 +0,0 @@
-start cmd /k "cd user-service && mvn spring-boot:run"
-start cmd /k "cd reservation-service && mvn spring-boot:run"
-start cmd /k "cd information-service && mvn spring-boot:run"
-start cmd /k "cd room-service && mvn spring-boot:run"
