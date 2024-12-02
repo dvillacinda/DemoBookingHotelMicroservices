b8c41b18f5df040e10e2ba9a2d6e06844bc6a248
dvillacinda <diegovillacinda.t@gmail.com>
2024-12-01 21:51:03 -0600

    updated pom file

---
+++ start-services-without-user.bat
@@ -0,0 +1,3 @@
+start cmd /k "cd reservation-service && mvn spring-boot:run"
+start cmd /k "cd information-service && mvn spring-boot:run"
+start cmd /k "cd room-service && mvn spring-boot:run"
---
+++ start-services.bat
@@ -0,0 +1,4 @@
+start cmd /k "cd user-service && mvn spring-boot:run"
+start cmd /k "cd reservation-service && mvn spring-boot:run"
+start cmd /k "cd information-service && mvn spring-boot:run"
+start cmd /k "cd room-service && mvn spring-boot:run"
