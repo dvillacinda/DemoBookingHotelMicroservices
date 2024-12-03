3e3d440e0080a4e703bccf4a402ed82020bcb990
dvillacinda <diegovillacinda.t@gmail.com>
2024-12-02 21:55:58 -0600

    add new properties and fallback route response

--- api-gateway/src/main/java/com/dv/booking/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/dv/booking/gateway/routes/Routes.java
@@ -4,13 +4,23 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+
+import java.net.URI;
+
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 
 @Configuration
 public class Routes {
+        // Services URL
 
         @Bean
         public RouterFunction<ServerResponse> informationService() {
@@ -18,6 +28,9 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/api/info/**"),
                                                 HandlerFunctions.http("http://localhost:8084"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("informationServiceCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .build();
         }
 
@@ -27,6 +40,9 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/api/reservation/**"),
                                                 HandlerFunctions.http("http://localhost:8081"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("reservationServiceCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .build();
         }
 
@@ -36,6 +52,9 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/api/room/**"),
                                                 HandlerFunctions.http("http://localhost:8082"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("roomServiceCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .build();
         }
 
@@ -45,8 +64,13 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/api/user/**"),
                                                 HandlerFunctions.http("http://localhost:8080"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("userServiceCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .build();
         }
+
+        // Swagger URL
 
         @Bean
         public RouterFunction<ServerResponse> informationServiceSwaggerRoute() {
@@ -54,6 +78,9 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/aggregate/information-service/v3/api-doc"),
                                                 HandlerFunctions.http("http://localhost:8084"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("informationServiceSwaggerCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .filter(setPath("/api-docs"))
                                 .build();
         }
@@ -64,6 +91,9 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/aggregate/reservation-service/v3/api-doc"),
                                                 HandlerFunctions.http("http://localhost:8081"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("reservationServiceSwaggerCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .filter(setPath("/api-docs"))
                                 .build();
         }
@@ -74,6 +104,9 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/aggregate/room-service/v3/api-doc"),
                                                 HandlerFunctions.http("http://localhost:8082"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("roomServiceSwaggerCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .filter(setPath("/api-docs"))
                                 .build();
         }
@@ -84,7 +117,21 @@ public class Routes {
                                 .route(
                                                 RequestPredicates.path("/aggregate/user-service/v3/api-doc"),
                                                 HandlerFunctions.http("http://localhost:8080"))
+                                .filter(CircuitBreakerFilterFunctions
+                                                .circuitBreaker("userServiceSwaggerCircuitBreaker",
+                                                                URI.create("forward:/fallbackRoute")))
                                 .filter(setPath("/api-docs"))
+                                .build();
+        }
+
+        //Fallback 
+
+        @Bean
+        public RouterFunction<ServerResponse> fallbackRoute() {
+                return route("fallbackRoute")
+                                .GET("/fallbackRoute",
+                                                request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                                                .body("Service Unavailable, place try again later"))
                                 .build();
         }
 
