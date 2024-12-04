08adf4cdb4f2914d9c79533f525db483ee5101cb
dvillacinda <diegovillacinda.t@gmail.com>
2024-12-03 19:43:48 -0600

    last config

--- room-service/src/main/java/com/dv/microservices/room/config/RestClientConfig.java
+++ room-service/src/main/java/com/dv/microservices/room/config/RestClientConfig.java
@@ -1,8 +1,13 @@
 package com.dv.microservices.room.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -18,10 +23,18 @@ public class RestClientConfig {
     public InformationClient roomClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(informationUrl)
+                .requestFactory(getClientHttpRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InformationClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        var clientHttpRequestFactory = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactory); 
     }
 
 }
