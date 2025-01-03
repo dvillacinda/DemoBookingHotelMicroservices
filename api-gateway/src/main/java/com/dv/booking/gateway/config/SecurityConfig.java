2afc38def24787de8052f6c07bbe19505864b765
dvillacinda <diegovillacinda.t@gmail.com>
2025-01-02 22:56:35 -0600

    change security

--- api-gateway/src/main/java/com/dv/booking/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/dv/booking/gateway/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     private final String[] freeResourceUrls = { "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-            "/swagger-resources/**", "/api-docs/**", "/aggregate/**","/actuator/prometheus" };
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus", "/api/info/*" };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
