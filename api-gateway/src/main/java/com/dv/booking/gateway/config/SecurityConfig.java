5c7347bed9bee8fcf0ee9cf524cf0c4d55fe2d16
dvillacinda <diegovillacinda.t@gmail.com>
2025-01-04 23:57:07 -0600

    updated api gateway

--- api-gateway/src/main/java/com/dv/booking/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/dv/booking/gateway/config/SecurityConfig.java
@@ -14,10 +14,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(freeResourceUrls)
-                .permitAll()
-                .anyRequest()
-                .authenticated())
+                .requestMatchers(freeResourceUrls).permitAll()
+                .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
